c9d87dfb072c14f671e9a8ae301bf0281e5e3808
Krisztian Kemenes <krisztian.kemenes@porsche-engineering.ro>
2025-07-11 11:30:43 +0300

    Adds support for fixed size views

--- Sources/ProportionalStack/ProportionalStack.swift
+++ Sources/ProportionalStack/ProportionalStack.swift
@@ -35,19 +35,80 @@ public struct ProportionalStack: Layout {
                               proposal: ProposedViewSize,
                               subviews: Subviews,
                               cache: inout ()) {
+        let totalFixedSize = subviews
+            .map { extractSize($0.sizeThatFits(proposal)) }
+            .filter { isCustom(size: $0, proposal: proposal) }
+            .reduce(0, +)
+        let totalContainerSpace = extractSize(bounds.size)
+        let totalRemainingContainerSpace = totalContainerSpace - totalFixedSize
+        let usableContainerSize = constructRect(
+            modifiedDimension: totalRemainingContainerSpace,
+            original: bounds
+        )
+        
         let totalSizeFactor = subviews
+            .filter {
+                !isCustom(size: extractSize($0.sizeThatFits(proposal)),
+                          proposal: proposal)
+            }
             .map(\.containerValues.proportion)
             .reduce(0, +)
         var origin = bounds.origin
         
+        
         for view in subviews {
-            let proportion = view.containerValues.proportion / totalSizeFactor
+            let viewSize = view.sizeThatFits(proposal)
+            let viewDimension = extractSize(view.sizeThatFits(proposal))
+            let isCustom = isCustom(size: viewDimension, proposal: proposal)
             
-            let size = self.getSize(containerSize: bounds.size,
-                                    proportion: proportion)
+            let size = if isCustom {
+                viewSize
+            } else {
+                getSize(containerSize: usableContainerSize.size,
+                        proportion: view.containerValues.proportion / totalSizeFactor)
+            }
+            
             view.place(at: origin, proposal: .init(size))
             origin = moveOrigin(origin, placedViewSize: size)
         }
+    }
+    
+    /// Get the size along the managed axis
+    private func extractSize(_ size: CGSize) -> CGFloat {
+        if direction == .vertical {
+            size.height
+        } else {
+            size.width
+        }
+    }
+    
+    private func constructRect(modifiedDimension: CGFloat,
+                               original bounds: CGRect) -> CGRect {
+        if direction == .vertical {
+            CGRect(origin: bounds.origin,
+                   size: CGSize(width: bounds.width,
+                                height: modifiedDimension))
+        } else {
+            CGRect(origin: bounds.origin,
+                   size: CGSize(width: modifiedDimension,
+                                height: bounds.height))
+        }
+    }
+    
+    /// Verify if the view has a custom size.
+    ///
+    /// Currently the condition for this is that it's size along the managed axis is smaller than
+    /// the containers size. This decision was made based on the assumption that if a view is smaller,
+    /// it has a fixed size and can't be freely modified, if it is larger, it wouldn't fix the container anyways
+    /// so we decide not to honor it's demands for space
+    private func isCustom(size viewSize: CGFloat, proposal: ProposedViewSize) -> Bool {
+        let containerDimension = if direction == .horizontal {
+            proposal.width ?? 0
+        } else {
+            proposal.height ?? 0
+        }
+        
+        return viewSize < containerDimension
     }
     
     private func moveOrigin(_ origin: CGPoint,
@@ -59,6 +120,7 @@ public struct ProportionalStack: Layout {
         }
     }
     
+    /// Construct a size based on the managed axis
     private func getSize(containerSize: CGSize,
                          proportion: Double) -> CGSize {
         return if direction == .horizontal {
@@ -75,7 +137,7 @@ public struct ProportionalStack: Layout {
     VStack {
         ProportionalStack(direction: .horizontal) {
             Color.red.proportion(3)
-            Color.green
+            Color.green.frame(width: 150)
             Color.blue.proportion(2)
         }
         .frame(width: 300, height: 300)
